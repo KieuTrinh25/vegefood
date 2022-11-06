6be99a2c464bf81f20a04dd433d5e13e9a79ee7e
unknown <conghung>
2022-11-06 08:10:15 +0700

    cartv3

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,5 +1,5 @@
 @extends('master')
-@section('title', __("web.cart_title"))
+@section('title', __('web.cart_title'))
 @section('content')
     <section id="home-section" class="hero">
         <div class="home-slider owl-carousel">
@@ -38,7 +38,9 @@
                                     <th class="cart-product-name">Images</th>
                                     <th class="cart-product-name">Product name</th>
                                     <th class="product-price">Price</th>
+                                    <th></th>
                                     <th class="quantity">Quantity</th>
+                                    <th></th>
                                     <th class="product-subtotal">Total</th>
                                     <th class="product-remove">Action</th>
                                 </tr>
@@ -61,23 +63,54 @@
                                                     {{ $orderDetail->product->name }}</a>
                                                 <p>{{ $orderDetail->product->description }}</p>
                                             </td>
-                                            <td class="product-price" >{{ currency_format( $orderDetail->product->price ) }}<span
+                                            <td class="product-price">
+                                                {{ currency_format($orderDetail->product->price) }}<span
                                                     class="amount"></span></td>
-                                            <td class="quantity">
-                                                <div class="cart-plus-minus">
-                                                    <input class="cart-plus-minus-box" value="{{ $orderDetail->quantity }}"
-                                                        type="text">
-                                                </div>
+                                            <td>
+                                                    <form action="{{ route('cart.update') }}" method="post">
+                                                        @csrf
+                                                        <input type="hidden" name="quantity" value="-1">
+                                                        <input type="hidden" name="orderDetailId"
+                                                            value="{{ $orderDetail->id }}">
+                                                        <button class="btn btn-light" type="submit">
+                                                            <h1>-</h1>
+                                                        </button>
+                                                    </form>
                                             </td>
-                                            <td class="product-subtotal"><span class="amount"> {{ currency_format( $orderDetail->product->price * $orderDetail->quantity ) }} </span></td>
+                                            <td class="quantity">
+
+                                                
+                                                <input type="text" id="quantity" name="quantity" class="form-control input-number" value="{{ $orderDetail->quantity }}">
+                                               
+                                            </td>
+                                            <td>
+                                                <form action="{{ route('cart.update') }}" method="post">
+                                                    @csrf
+                                                    <input type="hidden" name="quantity" value="1">
+                                                    <input type="hidden" name="orderDetailId"
+                                                        value="{{ $orderDetail->id }}">
+                                                    <button class="btn btn-light" type="submit">
+                                                       <h1>+</h1>
+                                                    </button>
+                                                </form>
+
+                                            </td>
+                                            <td class="product-subtotal"><span class="amount">
+                                                    {{ currency_format($orderDetail->total()) }}
+                                                </span></td>
                                             <td class="product-remove">
                                                 <form action="{{ route('cart.delete') }}" method="post">
                                                     @csrf
-                                                    <input type="hidden" name="order_detail_id" value="{{ $orderDetail->id }}">
-                                                    <button type="submit" >
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16" >
-                                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                                    <input type="hidden" name="order_detail_id"
+                                                        value="{{ $orderDetail->id }}">
+                                                    <button class="btn btn-light" type="submit">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="30"
+                                                            height="30" fill="currentColor" class="bi bi-x-circle"
+                                                            viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                            <path
+                                                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                                                         </svg>
                                                     </button>
                                                 </form>
@@ -94,83 +127,93 @@
         </div>
     </section>
     <div class="container">
-    <form action="{{ route('cart.store', $user->id) }}" class="info" method="post">
-        @csrf
-        <input type="hidden" name="orderId" value="{{ $order->id }}">
-        <div class="row justify-content-end">
-            <div class="col-md-7 mt-5 cart-wrap ftco-animate">
-                <div class="cart-total mb-3">
-                    <h3>Estimate shipping and tax</h3>
-                    <p>Enter your destination to get a shipping estimate</p>
-                    <div class="row">
-                        <div class="col-md-7">
-                            <div class="form-group">
-                                <label for="exampleInputName1">Full Name</label>
-                                <input name="name" class="form-control text-left px-3" value ="{{$user->name}}">
+        <form action="{{ route('cart.store', $user->id) }}" class="info" method="post">
+            @csrf
+            <input type="hidden" name="orderId" value="{{ $order->id }}">
+            <div class="row justify-content-end">
+                <div class="col-md-7 mt-5 cart-wrap ftco-animate">
+                    <div class="cart-total mb-3">
+                        <h3>Estimate shipping and tax</h3>
+                        <p>Enter your destination to get a shipping estimate</p>
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="form-group">
+                                    <label for="exampleInputName1">Full Name</label>
+                                    <input name="name" class="form-control text-left px-3" value="{{ $user->name }}">
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="form-group">
+                                    <label for="exampleInputName1">Phone Number</label>
+                                    <input name="phone" value="{{ old('phone') }}" type="text"
+                                        class="form-control text-left px-3" placeholder="phone">
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="exampleInputName1">Phone Number</label>
-                                <input  name="phone" value="{{ old('phone') }}" type="text" class="form-control text-left px-3" placeholder="phone">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input name="email" class="form-control text-left px-3"
+                                        value="{{ $user->email }}">
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-7">
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input name="email" class="form-control text-left px-3" value ="{{$user->email}}">
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <div class="form-group">
-                            <label for="country">Country</label>
-                                <select class="form-control" name="country" id="location">
-                                        @foreach($locationList as $location)
-                                            <option value="{{ $location->code}}">{{ $location->country }}</option>
+                            <div class="col-md-5">
+                                <div class="form-group">
+                                    <label for="country">Country</label>
+                                    <select class="form-control" name="country" id="location">
+                                        @foreach ($locationList as $location)
+                                            <option value="{{ $location->code }}">{{ $location->country }}</option>
                                         @endforeach
-                                </select>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="country">Address</label>
-                        <input  name="address" type="text" class="form-control text-left px-3" placeholder="Address">
-                    </div>
-                    <div class="form-group">
-                        <label for="country">Note</label>
-                        <input  name="note" type="text" class="form-control text-left px-3" placeholder="Note">
-                    </div>
-              </div>
-            </div>
-            <div class="col-md-5 mt-5 cart-wrap ftco-animate">
-                <div class="cart-total mb-3">
-                    <h3>Cart Totals</h3>
-                    <div class="row">
-                        <div class="col-md-6"><h6>Coupon Code</h6></div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input name="code" type="text" class="form-control text-left px-3" placeholder="voucher code">
-                            </div>
+                        <div class="form-group">
+                            <label for="country">Address</label>
+                            <input name="address" type="text" class="form-control text-left px-3"
+                                placeholder="Address">
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6"><h6>Ship</h6></div>
-                        <div class="col-md-6">
-                        <p id="ship" class="form-control text-left px-3"> </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6"><h4>Totals</h4></div>
-                        <div class="col-md-6">
-                            <p id="total"  class="form-control text-left px-3">{{currency_format ($total) }}</p>
+                        <div class="form-group">
+                            <label for="country">Note</label>
+                            <input name="note" type="text" class="form-control text-left px-3" placeholder="Note">
                         </div>
                     </div>
                 </div>
-               <button type="submit" class="btn btn-primary py-3 px-4">Proceed to checkout</button>
+                <div class="col-md-5 mt-5 cart-wrap ftco-animate">
+                    <div class="cart-total mb-3">
+                        <h3>Cart Totals</h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h6>Coupon Code</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input name="code" type="text" class="form-control text-left px-3"
+                                        placeholder="voucher code">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h6>Ship</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <p id="ship" class="form-control text-left px-3"> </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h4>Totals</h4>
+                            </div>
+                            <div class="col-md-6">
+                                <p id="total" class="form-control text-left px-3">{{ currency_format($total) }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary py-3 px-4">Proceed to checkout</button>
+                </div>
             </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="{{ asset('./css/icomoon.css') }}">
     <link rel="stylesheet" href="{{ asset('./css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('./css/search-form.scss') }}">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     @yield('stylesheet')
 </head>
 
